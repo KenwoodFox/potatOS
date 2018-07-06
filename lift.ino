368dc462abf0284f16f49b1e3843a324af208ffb
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2018-07-06 00:17:45 -0400

    meh

--- lift.ino
+++ lift.ino
@@ -24,7 +24,7 @@ void operation_lift(int LIFTSTATE)
 
   if(LIFTTIMEOUT > 20)
   {
-    Serial.println(0x1B);
+    //Serial.println(0x1B);
     //ansi.setForegroundColor(RED);
     Serial.println("Warning!! The PLC did not receive conformation that the walls have lifted to the correct height or at all! the PLC will continue to operate but check that the system is intact as damage is possible!");
     //ansi.setForegroundColor(WHITE);
--- vent.ino
+++ vent.ino
@@ -3,11 +3,11 @@ void operation_vent(int VENTSTATE)
   if(VENTSTATE = 1)
   {
     //on relay
-    Serial.print("turning on the vents");
+    Serial.println("turning on the vents");
   }
   else
   {
-    Serial.print("Turning off the vents");
+    Serial.println("Turning off the vents");
     //off relay
   }
   VENTCOMPLETE = true;
